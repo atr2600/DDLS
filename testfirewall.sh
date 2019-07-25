f6f6831a062ea3e916066187f560d9cf215b9c3b
Brandon Geraci <bran0847@d.umn.edu>
2019-07-24 18:42:22 -0700

    Testfirewall script

--- testfirewall.sh
+++ testfirewall.sh
@@ -1,21 +1,22 @@
 #!/bin/sh
 
 
-iptables -F 
-
-iptables -A FORWARD -p tcp --dport 6901 -m iprange --dst-range 172.0.0.0-172.255.255.255 -j ACCEPT
-iptables -A FORWARD -p udp --dport 6901 -m iprange --dst-range 172.0.0.0-172.255.255.255 -j ACCEPT
-iptables -A FORWARD -m iprange --dst-range 172.0.0.0-172.255.255.255 -j DROP
-iptables -A FORWARD -m state --state ESTABLISHED,RELATED -j ACCEPT
-
-iptables -A INPUT -p tcp --dport 6901 -m iprange --dst-range 172.0.0.0-172.255.255.255 -j ACCEPT
-iptables -A INPUT -p udp --dport 6901 -m iprange --dst-range 172.0.0.0-172.255.255.255 -j ACCEPT
-iptables -A INPUT -m iprange --dst-range 172.0.0.0-172.255.255.255 -j DROP
-iptables -A INPUT -m state --state ESTABLISHED,RELATED -j ACCEPT
-
-iptables -A OUTPUT -p tcp --sport 6901 -m iprange --src-range 172.0.0.0-172.255.255.255 -j ACCEPT
-iptables -A OUTPUT -p udp --sport 6901 -m iprange --src-range 172.0.0.0-172.255.255.255 -j ACCEPT
-iptables -A OUTPUT -m iprange --src-range 172.0.0.0-172.255.255.255 -j DROP
-iptables -A OUTPUT -m state --state ESTABLISHED,RELATED -j ACCEPT
+# iptables -F
 
 
+# iptables -I DOCKER-USER -m state --state ESTABLISHED,RELATED -j ACCEPT
+# iptables -I DOCKER-USER -m iprange --dst-range 172.0.0.0-172.255.255.255 -j DROP
+# iptables -I DOCKER-USER -m iprange --src-range 172.0.0.0-172.255.255.255 -j DROP
+# iptables -I DOCKER-USER -p tcp --dport 6901 -m iprange --dst-range 172.0.0.0-172.255.255.255 -j ACCEPT
+# iptables -I DOCKER-USER -p udp --dport 6901 -m iprange --dst-range 172.0.0.0-172.255.255.255 -j ACCEPT
+# iptables -I DOCKER-USER -p tcp --sport 6901 -m iprange --src-range 172.0.0.0-172.255.255.255 -j ACCEPT
+# iptables -I DOCKER-USER -p udp --sport 6901 -m iprange --src-range 172.0.0.0-172.255.255.255 -j ACCEPT
+
+
+iptables -A DOCKER-USER -p tcp --dport 6901 -m iprange --dst-range 172.0.0.0-172.255.255.255 -j ACCEPT
+iptables -A DOCKER-USER -p udp --dport 6901 -m iprange --dst-range 172.0.0.0-172.255.255.255 -j ACCEPT
+iptables -A DOCKER-USER -p tcp --sport 6901 -m iprange --src-range 172.0.0.0-172.255.255.255 -j ACCEPT
+iptables -A DOCKER-USER -p udp --sport 6901 -m iprange --src-range 172.0.0.0-172.255.255.255 -j ACCEPT
+iptables -A DOCKER-USER -m iprange --dst-range 172.0.0.0-172.255.255.255 -j DROP
+iptables -A DOCKER-USER -m iprange --src-range 172.0.0.0-172.255.255.255 -j DROP
+iptables -A DOCKER-USER -m state --state ESTABLISHED,RELATED -j ACCEPT
